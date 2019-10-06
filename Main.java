f079e8aa2ac7a4eb1798b3d9459a0f33b9b6e6cf
Tore003 <56234021+Tore003@users.noreply.github.com>
2019-10-06 17:38:52 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Scanner;
+public class Unidad4 {
+
+   
+    public static void main(String[] args) throws IOException 
+    {
+        BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
+        int numero=0;
+        numero = String.valueOf(numero).length(); //
+        int reverse=0;
+        int sum=0;
+        System.out.println("Ingrese un numero de 4 digitos");
+        numero = Integer.parseInt(bufEntrada.readLine());
+        int temp = numero;
+             while(numero>0)
+            {    //formula para determinar si es capicua
+                reverse=numero%10;   
+                sum=(sum*10)+ reverse;     
+                numero=numero/10;    
+            }   
+             if(temp==sum)
+             {   
+                System.out.println(numero + " El numero ingresado es capicua ");
+             }    
+             else
+             {    
+                System.out.println(numero + " El numero ingresado no es capicua");    
+             }  
+    }  
+}   
