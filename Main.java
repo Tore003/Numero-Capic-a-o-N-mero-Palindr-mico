c75d9e98e4ca15bf5a52b2023757f637e2a2cefc
Tore003 <56234021+Tore003@users.noreply.github.com>
2019-10-06 17:41:08 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Scanner;
-public class Unidad4 {
+public class NumeroCoP {
 
    
     public static void main(String[] args) throws IOException 
